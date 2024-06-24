55d5b4aab671d779fa0ad21ce1b8057e8de1a56d
katdev16 <you@example.com>
2024-06-25 00:57:10 +0200

    some functionality for task manager

---
+++ Main/src/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    public String task;
+
+
+    String add_task(String task){
+        return task;
+    }
+
+    boolean task_completion(){
+        return true;
+    }
+
+    void reason(){
+
+        System.out.println("Reason: ");
+    }
+
+
+
+}
