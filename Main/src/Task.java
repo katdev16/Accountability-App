9f35c0b3da9e80c1528c9374cf376a61b656168b
kdhlamini023b <kdhlamini023b@student.wethinkcode.co.za>
2024-06-25 09:53:09 +0200

    creating methods for task class

--- Main/src/Task.java
+++ Main/src/Task.java
@@ -1,12 +1,42 @@
+import java.time.Duration;
+import java.util.Scanner;
+
 public class Task {
     public String task;
+    public int Duration;
+    public String completion;
 
 
-    String add_task(String task){
+    public String add_task(){
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter task");
+
+        this.task = myObj.nextLine();  // Read user input
+        System.out.println("Task added: " + task);  // Output user input
         return task;
     }
 
+    public int Duration(){
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter completion time");
+        String input = myObj.nextLine();
+
+        Integer.parseInt(input);
+
+        this.Duration = Integer.parseInt(input);;  // Read user input
+        System.out.println("Duration: " + Duration+"hr");
+        return Duration;
+    }
+
+
     boolean task_completion(){
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Completion(y/n)");
+        String input = myObj.nextLine();
+
+
+
+
         return true;
     }
 
@@ -15,6 +45,14 @@ public class Task {
         System.out.println("Reason: ");
     }
 
-
+    public void main(String[] args) {
+        add_task();
+        Duration();
+    }
 
 }
+
+
+
+
+
