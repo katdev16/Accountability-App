33056c99999b4b101a788ea04463dcd3dbdebcc1
kdhlamini023b <kdhlamini023b@student.wethinkcode.co.za>
2024-06-25 11:59:38 +0200

    made users class and added som methods

--- Main/src/Task.java
+++ Main/src/Task.java
@@ -83,7 +83,6 @@ public class Task {
         task_completion();
         task_viewer();
 
-
     }
 
 }
---
+++ Main/src/Users.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Users {
+    private String username;
+    private String email;
+    private String password;
+
+    public String add_username(){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username");
+        String input = myObj.nextLine();
+        System.out.println("Username: " + input);
+        return this.username;
+    }
+
+    public String add_email(){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter email");
+        String input = myObj.nextLine();
+        System.out.println("Email: " + input);
+        return this.email;
+    }
+
+    public String Make_password(){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter password");
+        String input = myObj.nextLine();
+        System.out.println("Password: " + input);
+        return this.password;
+    }
+
+
+
+    public void main(String[] args) {
+        add_username();
+        add_email();
+        Make_password();
+
+    }
+
+
+}
