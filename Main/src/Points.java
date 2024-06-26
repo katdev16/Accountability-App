fcb735051b8d9904ca95c6ce7b0c7da562d732dd
katdev16 <katlegodhlamini2003@gmail.com>
2024-06-27 01:50:47 +0200

    setter and getter for points class

--- Main/src/Points.java
+++ Main/src/Points.java
@@ -1,6 +1,20 @@
 public class Points {
     private int number;
 
+//    public int task_point(){
+//        this.number+=1;
+//        return number;
+//    }
 
+//    public int points_for_completion(){
+//        number
+//    }
+    public void setNumber(){
+        number=1;
+    }
+
+    public int getNumber(){
+        return number;
+    }
 }
 
