c950ae249f1c914a1f7e9087e204299c6ada3a8a
katdev16 <katlegodhlamini2003@gmail.com>
2024-06-27 01:29:02 +0200

    enter new task added

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -13,5 +14,20 @@ public class App {
         task.Duration();
         task.task_completion();
         task.task_viewer();
+
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Add new task(y/n :");
+        String input = myObj.nextLine();
+        while (input.equals("y")){
+
+                task.add_task();
+                task.Duration();
+                task.task_completion();
+                task.task_viewer();
+
+        }
+
+
+
     }
 }
