65fd67ccad97ebef7395c4ac78e1c6b200f76602
katdev16 <katlegodhlamini2003@gmail.com>
2024-12-08 13:14:44 +0200

    springboot

---
+++ src/main/java/com/katdev/accountabilityapp/AccountabilityApp.java
@@ -0,0 +1,11 @@
+package com.katdev.accountabilityapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountabilityApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountabilityApp.class, args);
+    }
+}
