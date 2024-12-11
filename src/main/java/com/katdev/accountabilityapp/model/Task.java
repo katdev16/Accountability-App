d6df030b935192b6b2bb4a7f64a333eec50747f1
katdev16 <katlegodhlamini2003@gmail.com>
2024-12-11 22:54:06 +0200

    model updated

--- src/main/java/com/katdev/accountabilityapp/model/Task.java
+++ src/main/java/com/katdev/accountabilityapp/model/Task.java
@@ -1,147 +1,56 @@
 package com.katdev.accountabilityapp.model;
 
-import com.katdev.accountabilityapp.service.Points;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 @Entity
 public class Task {
 
-
-    public Task(){
-
-        Duration=0;
-        completion=false;
-//        Reason=null;
-    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String user;
-    private String task;
-    private int Duration;
-    private boolean completion;
+    private Long id;
 
-    public boolean isCompletion() {
-        return completion;
+    private String title;
+    private String description;
+    private String status;
+
+    public Task() {
+        this.status = "Pending"; // Default value for status
     }
 
-    public void setCompletion(boolean completion) {
-        this.completion = completion;
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
     }
 
-    public int getDuration() {
-        return Duration;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setDuration(int duration) {
-        Duration = duration;
+    public String getTitle() {
+        return title;
     }
 
-    public String getTask() {
-        return task;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public void setTask(String task) {
-        this.task = task;
+    public String getDescription() {
+        return description;
     }
 
-    public String getUser() {
-        return user;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public void setUser(String user) {
-        this.user = user;
+    public String getStatus() {
+        return status;
     }
 
-//    private String Reason;
-
-//    private ArrayList<String> task_list;
-//
-//    public Points points = new Points();
-
-//    public String add_task(){
-//        this.task_list= new ArrayList<String>();
-//
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("Enter task");
-//
-//        this.task = myObj.nextLine();  // Read user input
-//        System.out.println("com.katdev.accountabilityapp.model.Task added: " + task);  // Output user input
-//        setNumber();
-//        task_list.add(task);
-////        System.out.println(task_list);
-//
-//        return task;
-//    }
-
-//    public int Duration(){
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("Enter completion time");
-//        String input = myObj.nextLine();
-//
-//        Integer.parseInt(input);
-//
-//        this.Duration = Integer.parseInt(input);;  // Read user input
-//        System.out.println("Duration: " + Duration+"hr");
-//        return Duration;
-//    }
-
-
-//    boolean task_completion(){
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("Completion(y/n)");
-//        String input = myObj.nextLine();
-//
-//        this.completion=input;
-//
-//
-//        if(input.equals("y")){
-//            System.out.println("DONE");
-//            setUser_points(getNumber());
-//
-//
-//        }else if(input.equals("n")){
-//            System.out.println("NOT DONE");
-//            reason();
-//        }
-//        return true;
-//    }
-
-//    void reason(){
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("Enter reason for not completing");
-//        String input = myObj.nextLine();
-//
-//        this.Reason= input;
-//        System.out.println("Reason: "+Reason);
-//    }
-
-//    public ArrayList<String> task_dashboard(){
-//
-//
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//        System.out.println("View task dashboard(y/n)");
-//        String input = myObj.nextLine();
-//        if(input.equals("y")){
-//            System.out.println(task_list);
-//
-//
-//
-//            System.out.println("com.katdev.accountabilityapp.service.Points :"+getUser_points());
-//
-//        }
-//
-//        return this.task_list;
-//    }
-
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
-
-
-
-
-
